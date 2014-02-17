3a305e81542720d42b5c002a06d5c8eeb591b3a6
Engin KIRMACI <mail@enginkirmaci.com>
2014-02-17 13:56:04 +0200

    ADDED MATHS AND EXTENSIONS

---
+++ NISDKExtendedEffects/Entities/IShapeOptimizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace NISDKExtendedEffects.Entities
+{
+    public interface IShapeOptimizer
+    {
+        /// <summary>
+        /// Optimize specified shape.
+        /// </summary>
+        ///
+        /// <param name="shape">Shape to be optimized.</param>
+        ///
+        /// <returns>Returns final optimized shape, which may have reduced amount of points.</returns>
+        ///
+        List<Point> OptimizeShape(List<Point> shape);
+    }
+}
